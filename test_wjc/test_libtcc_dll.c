7ea09ebc526165993638f56cf3a6c5281a0eb889
wanjochan <wanjochan@gmail.com>
2018-09-10 23:43:21 +0800

    test_libtcc_dll.exe

--- test_wjc/test_libtcc_dll.c
+++ test_wjc/test_libtcc_dll.c
@@ -1,15 +1,36 @@
+#include <windows.h>
+
 #include <stdio.h>
-#include "tcc.h"
-#include "libtcc.c"
+//#include "tcc.h"
+//#include "libtcc.c"
+//
+////#include "tcctools.c"
+//
+int libtcc_dll_main(int argc0, char **argv0);
 
-//#include "tcctools.c"
-
-int main()
+int main(int argc0, char **argv0)
 {
-    TCCState *s;
-    printf("Hello World\n");
-    return 0;
+	//TCCState *s;
+	//printf("Hello World\n");
+	//return 0;
+	return libtcc_dll_main(argc0,argv0);
 }
+
+
+//int libtcc_dll_main(int argc0, char **argv0)
+
+//__declspec(dllimport) extern const char *hello_data;
+
+//int WINAPI WinMain(
+//    HINSTANCE hInstance,
+//    HINSTANCE hPrevInstance,
+//    LPSTR     lpCmdLine,
+//    int       nCmdShow)
+//{
+//    hello_data = "Hello World!";
+//    hello_func();
+//    return 0;
+//}
 
 //#include <windows.h>
 //
--- wjc_build_with_docker_bin.sh
+++ wjc_build_with_docker_bin.sh
@@ -35,7 +35,8 @@ cp i386-win32-libtcc1.a ../test_wjc/lib/libtcc1.a
 
 ./i386-win32-tcc -o ../test_wjc/tcc_i386-win32.exe ../tcc.c
 
-./i386-win32-tcc -shared -rdynamic -o ../test_wjc/libtcc_i386-win32.dll ../libtcc.c
+#./i386-win32-tcc -shared -rdynamic -o ../test_wjc/libtcc_i386-win32.dll ../libtcc.c
+./i386-win32-tcc -I.. -shared -rdynamic -o ../test_wjc/libtcc_dll.dll ../test_wjc/libtcc_dll.c
 
 ./i386-win32-tcc -impdef ../test_wjc/libtcc_i386-win32.dll
 mv *.def ../test_wjc/
@@ -43,7 +44,8 @@ mv *.def ../test_wjc/
 ./i386-win32-tcc -o ../test_wjc/test_c_w32.exe ../test_wjc/test_c.c
 
 # using the cross one ok?
-./i386-win32-tcc -o ../test_wjc/test_libtcc_dll.exe ../test_wjc/test_libtcc_dll.c -I..
+#./i386-win32-tcc -o ../test_wjc/test_libtcc_dll.exe ../test_wjc/test_libtcc_dll.c -I..
+./i386-win32-tcc -o ../test_wjc/test_libtcc_dll.exe ../test_wjc/test_libtcc_dll.c ../test_wjc/libtcc_dll.dll
 
 cp -Rf ../include ../test_wjc
 cp -R ../win32/include/* ../test_wjc/include/
@@ -52,3 +54,4 @@ ls -al ../test_wjc/
 
 #echo try test_libtcc_dll.bat in win32
 
+echo test_libtcc_dll.exe should be able to run now
