90c333adc362c0fd2b24509a0a59d79198f066d5
wanjochan <wanjochan@gmail.com>
2018-09-10 23:59:05 +0800

    ok inside win32 too

--- test_wjc/test_libtcc_dll.bat
+++ test_wjc/test_libtcc_dll.bat
@@ -2,6 +2,8 @@
 
 @rem tcc_i386-win32.exe -I..\win32\include -I..\include -I.. -I..\win32\include\winapi -DTCC_TARGET_I386 -Wall -g -O2 -run test_libtcc_dll.c 
 
-@tcc_i386-win32.exe -I..\win32\include -I..\include -I.. -I..\win32\include\winapi -DTCC_TARGET_I386 test_libtcc_dll.c 
+@tcc_i386-win32.exe -o ../test_wjc/test_libtcc_dll.exe ../test_wjc/test_libtcc_dll.c ../test_wjc/libtcc_dll.dll
+
+@tcc_i386-win32.exe -I..\win32\include -I..\include -I.. -I..\win32\include\winapi -DTCC_TARGET_I386 test_libtcc_dll.c libtcc_dll.dll
 
 @test_libtcc_dll.exe %*
