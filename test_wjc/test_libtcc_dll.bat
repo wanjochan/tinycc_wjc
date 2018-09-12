0c8bf00bddb6f40de7d43dac43d71303b99186ca
wanjochan <wanjochan@gmail.com>
2018-09-12 16:01:54 +0800

    failed...

--- test_wjc/test_libtcc_dll.bat
+++ test_wjc/test_libtcc_dll.bat
@@ -2,10 +2,9 @@
 
 @rem tcc_i386-win32.exe -I..\win32\include -I..\include -I.. -I..\win32\include\winapi -DTCC_TARGET_I386 -Wall -g -O2 -run test_libtcc_dll.c 
 
-@tcc_i386-win32.exe -o ../test_wjc/test_libtcc_dll.exe ../test_wjc/test_libtcc_dll.c ../test_wjc/libtcc_dll.dll
+@tcc_i386-win32.exe -o ../test_wjc/test_libtcc_dll.exe ../test_wjc/test_libtcc_dll.c ../test_wjc/libtcc.dll
 
-@rem @tcc_i386-win32.exe -I..\win32\include -I..\include -I.. -I..\win32\include\winapi -DTCC_TARGET_I386 test_libtcc_dll.c libtcc_dll.dll
-@tcc_i386-win32.exe -I..\win32\include -I..\include -I.. -I..\win32\include\winapi -DTCC_TARGET_I386 -DTCC_TARGET_PE test_libtcc_dll.c libtcc_dll.def
+@tcc_i386-win32.exe -I..\win32\include -I..\include -I.. -I..\win32\include\winapi -DTCC_TARGET_I386 -DTCC_TARGET_PE test_libtcc_dll.c libtcc.def
 
 @test_libtcc_dll.exe %*
 
--- wjc_build_with_docker_bin.sh
+++ wjc_build_with_docker_bin.sh
@@ -29,12 +29,6 @@ chmod +x *.sh
 ./tcc_run.sh examples/ex3.c 30
 
 
-# TODO rebuild  "i386-win32-tcc" with libtcc1.a in same folder.....
-
-
-
-
-
 # i386 cross test
 
 #cp i386-libtcc1.a ../test_wjc/
@@ -45,17 +39,15 @@ chmod +x *.sh
 
 ./i386-win32-tcc -DTCC_TARGET_I386 -DTCC_TARGET_PE -o ../test_wjc/tcc_i386-win32.exe ../tcc.c
 
-#./i386-win32-tcc -DTCC_TARGET_I386 -DTCC_TARGET_PE -I.. -shared -rdynamic -o ../test_wjc/libtcc_dll.dll ../test_wjc/libtcc_dll.c
-# tmp TCC_LIB_DLL, to fix later
 ./i386-win32-tcc -DTCC_TARGET_I386 -DTCC_TARGET_PE -DTCC_LIB_DLL -I.. -shared -rdynamic -o ../test_wjc/libtcc.dll ../tcc.c
 
-./i386-win32-tcc -impdef ../test_wjc/libtcc_dll.dll
+./i386-win32-tcc -impdef ../test_wjc/libtcc.dll
 mv *.def ../test_wjc/
 
-#./i386-win32-tcc -DTCC_TARGET_I386 -DTCC_TARGET_PE -o ../test_wjc/test_libtcc_dll.exe ../test_wjc/test_libtcc_dll.c ../test_wjc/libtcc_dll.dll
 ./i386-win32-tcc -DTCC_TARGET_I386 -DTCC_TARGET_PE -o ../test_wjc/test_libtcc_dll.exe ../test_wjc/test_libtcc_dll.c ../test_wjc/libtcc.dll
 
 cp -R lib ../test_wjc/
+
 cp -Rf ../include ../test_wjc
 cp -R ../win32/include/* ../test_wjc/include/
 
