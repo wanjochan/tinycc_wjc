c42a6851deed57cc8df37dc6e1f4771cb4c3c883
wanjochan <wanjochan@gmail.com>
2018-10-31 15:08:41 +0800

    #define RTLD_LAZY     0x1 #define RTLD_NOW      0x2 #define RTLD_NOLOAD   0x4 #define RTLD_GLOBAL   0x100 #define RTLD_NODELETE 0x1000 #define RTLD_LOCAL    0x0

--- tcc_dl.h
+++ tcc_dl.h
@@ -97,12 +97,20 @@ extern char *dlerror (void);
 #endif /* not POSIX */
 
 #else//!__APPLE__
-#define RTLD_LAZY   1
-#define RTLD_NOW    2
-#define RTLD_NOLOAD 4
-#define RTLD_NODELETE 4096
-#define RTLD_GLOBAL 256
-#define RTLD_LOCAL  0
+
+#define RTLD_LAZY     0x1
+#define RTLD_NOW      0x2
+#define RTLD_NOLOAD   0x4
+#define RTLD_GLOBAL   0x100
+#define RTLD_NODELETE 0x1000
+#define RTLD_LOCAL    0x0
+
+//#define RTLD_LAZY   1
+//#define RTLD_NOW    2
+//#define RTLD_NOLOAD 4
+//#define RTLD_NODELETE 4096
+//#define RTLD_GLOBAL 256
+//#define RTLD_LOCAL  0
 
 #define RTLD_NEXT    ((void *)-1)
 #define RTLD_DEFAULT ((void *)0)
